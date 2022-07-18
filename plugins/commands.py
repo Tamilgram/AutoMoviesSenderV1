c2e5ce92f412778917eb0e7ee71cbdb7e38fee6f
Tamilgram <107699896+Tamilgram@users.noreply.github.com>
2022-07-18 06:31:55 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -8,7 +8,7 @@ from pyrogram.errors import ChatAdminRequired, FloodWait
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from database.ia_filterdb import Media, get_file_details, unpack_new_file_id
 from database.users_chats_db import db
-from info import CHANNELS, ADMINS, AUTH_CHANNEL, LOG_CHANNEL, PICS, BATCH_FILE_CAPTION, CUSTOM_FILE_CAPTION, PROTECT_CONTENT
+from info import CHANNELS, ADMINS, AUTH_CHANNEL, LOG_CHANNEL, PICS, BATCH_FILE_CAPTION, CUSTOM_FILE_CAPTION, PROTECT_CONTENT, CHANNEL_NAME, CHANNEL_LINK
 from utils import get_settings, get_size, is_subscribed, save_group_settings, temp
 from database.connections_mdb import active_connection
 import re
@@ -42,7 +42,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('🎬 𝖩𝗈𝗂𝗇 𝖳𝖺𝗆𝗂𝗅𝖱𝗈𝖺𝗋𝗌 𝖭𝗈𝗐 🎬', url=f'http://t.me/Tamil_Roars')
+            InlineKeyboardButton('🎬 𝖩𝗈𝗂𝗇 {CHANNEL_NAME} 𝖭𝗈𝗐 🎬', url=f'{CHANNEL_LINK}')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
@@ -61,7 +61,7 @@ async def start(client, message):
         btn = [
             [
                 InlineKeyboardButton(
-                    "🎬 Jᴏɪɴ TᴀᴍɪʟRᴏᴀʀs Cʜᴀɴɴᴇʟ 🎬", url=invite_link.invite_link
+                    "🎬 Jᴏɪɴ {CHANNEL_NAME} Cʜᴀɴɴᴇʟ 🎬", url=invite_link.invite_link
                 )
             ]
         ]
