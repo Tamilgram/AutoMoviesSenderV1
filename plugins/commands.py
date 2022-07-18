376afc64c59d7db38a9ecd1ce70a34a125d66c14
Tamilgram <107699896+Tamilgram@users.noreply.github.com>
2022-07-18 07:12:46 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -42,7 +42,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('🎬 𝖩𝗈𝗂𝗇 {CHANNEL_NAME} 𝖭𝗈𝗐 🎬', url=f'{CHANNEL_LINK}')
+            InlineKeyboardButton('🎬 𝖩𝗈𝗂𝗇 Main 𝖭𝗈𝗐 🎬', url=invite_link.invite_link
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
@@ -61,7 +61,7 @@ async def start(client, message):
         btn = [
             [
                 InlineKeyboardButton(
-                    "🎬 Jᴏɪɴ {CHANNEL_NAME} Cʜᴀɴɴᴇʟ 🎬", url=invite_link.invite_link
+                    "🎬 Jᴏɪɴ Main Cʜᴀɴɴᴇʟ 🎬", url=invite_link.invite_link
                 )
             ]
         ]
