ffb8568bb27fa4055332b6f20c4070857f23becb
Tamilgram <107699896+Tamilgram@users.noreply.github.com>
2022-08-13 17:19:52 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -75,7 +75,7 @@ async def start(client, message):
                 btn.append([InlineKeyboardButton(" ↺ Try Again ↺", url=f"https://t.me/{temp.U_NAME}/{message.command[1]}")])
         await client.send_message(
             chat_id=message.from_user.id,
-            text="**Please Join My Updates Channel to use this Bot!**",
+            text="**{CUSTOM_FORCESUB}**",
             reply_markup=InlineKeyboardMarkup(btn),
             parse_mode="markdown"
             )
